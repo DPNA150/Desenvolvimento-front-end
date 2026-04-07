d4caea4dacb78ddf0a4890f60434f73e29a54f6c
AJS <arthurdesa204@gmail.com>
2026-04-07 08:53:44 -0300

    Update scriptLogin.js
    Atualização relacionada ao mini-projeto 1

--- scriptLogin.js
+++ scriptLogin.js
@@ -1,21 +1,33 @@
-function toggleForm() {
+document.addEventListener("DOMContentLoaded", function () {
+
+    const loginRadio = document.getElementById('log');
+    const cadastroRadio = document.getElementById('cad');
     const loginForm = document.getElementById('login-form');
     const signupForm = document.getElementById('signup-form');
     const signupForm2 = document.getElementById('signup-form2');
     const formTitle = document.getElementById('form-title');
 
-    if (loginForm.style.display === 'none') {
-        loginForm.style.display = 'block';
-        signupForm.style.display = 'none';
-        signupForm2.style.display = 'none';
-        formTitle.textContent = 'Login';
-    } else {
-        loginForm.style.display = 'none';
-        signupForm.style.display = 'block';
-        signupForm2.style.display = 'none';
-        formTitle.textContent = 'Cadastro';
+    function toggleForm() {
+
+        if (loginRadio.checked) {
+            loginForm.style.display = 'block';
+            signupForm.style.display = 'none';
+            signupForm2.style.display = 'none';
+            formTitle.textContent = 'Login';
+        }
+
+        if (cadastroRadio.checked) {
+            loginForm.style.display = 'none';
+            signupForm.style.display = 'block';
+            signupForm2.style.display = 'none';
+            formTitle.textContent = 'Cadastro';
+
+        }
     }
-}
+
+    loginRadio.addEventListener('change', toggleForm);
+    cadastroRadio.addEventListener('change', toggleForm);
+});
 
 // LOGIN
 document.getElementById('login-form').addEventListener('submit', function (event) {
@@ -47,7 +59,6 @@ document.getElementById('signup-form').addEventListener('submit', function (even
         return;
     }
 
-    // Ir para próxima etapa
     this.style.display = 'none';
     document.getElementById('signup-form2').style.display = 'block';
 });
